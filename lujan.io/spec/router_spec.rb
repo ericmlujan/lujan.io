a29dbd11c7f7a1564add688705157443aa5d30ed
Eric Lujan <lujan@mit.edu>
2021-09-04 22:27:31 -0700

    Run prettier

--- lujan.io/spec/router_spec.rb
+++ lujan.io/spec/router_spec.rb
@@ -3,33 +3,34 @@ require 'rack'
 require_relative '../router'
 
 def make_mock_request(uri)
-  env = Rack::MockRequest::env_for(uri)
+  env = Rack::MockRequest.env_for(uri)
   Rack::Request.new(env)
 end
 
-RSpec.describe Router, "#route" do
-  context "no routes loaded" do
-    it "returns a 404 error" do
+RSpec.describe Router, '#route' do
+  context 'no routes loaded' do
+    it 'returns a 404 error' do
       router = Router.new
-      request = make_mock_request("/")
+      request = make_mock_request('/')
       response = router.route(request)
 
       expect(response.status).to eq(404)
-      expect(response.body).to eq(["not found"])
+      expect(response.body).to eq(['not found'])
     end
   end
 
-  context "GET / is defined" do
-    it "returns an ok status" do
-      body_text = "i am root"
+  context 'GET / is defined' do
+    it 'returns an ok status' do
+      body_text = 'i am root'
 
-      router = Router.new do
-        get '/' do
-          body_text
+      router =
+        Router.new do
+          get '/' do
+            body_text
+          end
         end
-      end
 
-      request = make_mock_request("/")
+      request = make_mock_request('/')
       response = router.route(request)
 
       expect(response.status).to eq(200)
